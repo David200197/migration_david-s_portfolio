67590d11549769e092773a80d0fb00e34d4410ae
David200197 <david.alfonso999999@gmail.com>
2025-08-14 17:07:53 -0400

    feat: Add maintenance page and language switcher
    - Implemented a new maintenance page with a link to the home page.
    - Added a LanguageSwitcher component to allow users to switch between English and Spanish.
    - Updated navigation links to point to the new maintenance page.
    - Introduced a new SVG icon for language selection.
    - Modified existing JSON data to reflect the new routing structure.

--- src/modules/portfolio/view/MaintancePage.tsx
+++ src/modules/portfolio/view/MaintancePage.tsx
@@ -3,12 +3,17 @@ import { BackgroundParticles } from "../components/BackgroundParticles";
 import { BlurScreen } from "../components/BlurScreen";
 import Link from "next/link";
 
-export const PageNotFound = () => (
+export const MaintancePage = () => (
   <div className="h-[100vh]">
     <div className="w-full h-full flex">
       <div className="m-auto flex flex-col items-center">
-        <img src="/404.svg" alt="404" width={200} height={200} />
-        <p className="text-3xl">Page not Found</p>
+        <img
+          src="/maintenance.svg"
+          alt="maintenance"
+          width={200}
+          height={200}
+        />
+        <p className="text-3xl">This Page is Under Maintenance</p>
         <Button className="text-center mt-2">
           <Link href="/en">Go to Home</Link>
         </Button>
